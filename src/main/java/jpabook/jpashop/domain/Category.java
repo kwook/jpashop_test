ce006d2aecc27a2dae9c20ab0bf599d68cf999a5
kwook <kwook@isu.co.kr>
2023-02-06 08:39:36 +0900

    2023.02.06 수업내용 커밋

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"), // items가 조인하는 것
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID") // 반대쪽이 조인하는 것
+    )
+    private List<Item> items = new ArrayList<>();
+}
